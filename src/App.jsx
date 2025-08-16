57a529583c57614e175c481375e4738b206036a7
sriram A <sriram.coder2004@gmail.com>
2025-08-16 09:35:07 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+
 // import React from "react";
 // import {
 //   BrowserRouter as Router,
@@ -11,7 +13,6 @@
 // import "./styles/global.css";
 // import Login from "./Pages/Login";
 // import Register from "./Pages/Register";
-
 
 // import AllGuestPayments from "./Pages/AllGuestPayments";
 
@@ -77,6 +78,7 @@
 //     <AuthProvider>
 //       <Router>
 //         <Routes>
+//           {/* Public routes */}
 //           <Route
 //             path="/login"
 //             element={
@@ -93,17 +95,19 @@
 //               </PublicRoute>
 //             }
 //           />
+
+//           {/* Private routes */}
 //           <Route
 //             path="/*"
 //             element={
 //               <PrivateRoute>
 //                 <Routes>
+//                   {/* Default redirect */}
+//                   <Route path="/" element={<Navigate to="/hotel-RoomStatus" replace />} />
+
 //                   {/* Hotel routes */}
 //                   <Route path="/hotel" element={<HotelUserProfile />} />
 //                   <Route path="/hotel-Page" element={<HotelPage />} />
-
-                 
-
 //                   <Route path="/hotel-Rooms" element={<HotelRooms />} />
 //                   <Route
 //                     path="/hotel-GuestDetails"
@@ -115,7 +119,8 @@
 //                     path="/hotel-RoomStatus"
 //                     element={<HotelRoomStatus />}
 //                   />
-//                   {/* Homestays routes */}
+
+//                   {/* Homestay routes */}
 //                   <Route path="/homestay" element={<HomestayUserProfile />} />
 //                   <Route path="/homestay-Page" element={<HomestaysPage />} />
 //                   <Route path="/homestay-Rooms" element={<HomestaysRooms />} />
@@ -136,20 +141,11 @@
 //                     element={<HomestaysRoomStatus />}
 //                   />
 
-
-
-
-//  {/* ----------- */}
-
-//               <Route
-//                 path="/AllGuestPayments"
-//                 element={<AllGuestPayments />}
-//               />
-
-//               {/* ----------- */}
-
-
-
+//                   {/* Other routes */}
+//                   <Route
+//                     path="/AllGuestPayments"
+//                     element={<AllGuestPayments />}
+//                   />
 //                 </Routes>
 //               </PrivateRoute>
 //             }
@@ -161,15 +157,6 @@
 // }
 
 // export default App;
-
-
-
-
-
-
-
-
-
 
 
 
@@ -196,10 +183,10 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 
 import AllGuestPayments from "./Pages/AllGuestPayments";
-
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 
+// Hotel Admin
 import HotelUserProfile from "./Hotel_Admin_panel/UserProfile";
 import HotelPage from "./Hotel_Admin_panel/Hotels";
 import HotelRooms from "./Hotel_Admin_panel/Rooms";
@@ -208,6 +195,7 @@ import HotelPayments from "./Hotel_Admin_panel/PaymentaPage";
 import HotelReviews from "./Hotel_Admin_panel/Reviews";
 import HotelRoomStatus from "./Hotel_Admin_panel/RoomStatus";
 
+// Homestay Admin
 import HomestayUserProfile from "./Homestay_Admin_panel/UserProfile";
 import HomestaysPage from "./Homestay_Admin_panel/Homestay";
 import HomestaysRooms from "./Homestay_Admin_panel/Rooms";
@@ -219,9 +207,7 @@ import HomestaysRoomStatus from "./Homestay_Admin_panel/RoomStatus";
 function Layout({ children }) {
   const [sidebarOpen, setSidebarOpen] = React.useState(false);
 
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
 
   return (
     <div className="App">
@@ -236,28 +222,21 @@ function Layout({ children }) {
 
 function PrivateRoute({ children }) {
   const { user, loading } = useAuth();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
+  if (loading) return <div>Loading...</div>;
   return user ? <Layout>{children}</Layout> : <Navigate to="/login" replace />;
 }
 
 function PublicRoute({ children }) {
   const { user, loading } = useAuth();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
+  if (loading) return <div>Loading...</div>;
   return user ? <Navigate to="/" replace /> : children;
 }
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      {/* IMPORTANT: basename for GitHub Pages */}
+      <Router basename="/sonachala-Adminfrontend">
         <Routes>
           {/* Public routes */}
           <Route
@@ -284,7 +263,10 @@ function App() {
               <PrivateRoute>
                 <Routes>
                   {/* Default redirect */}
-                  <Route path="/" element={<Navigate to="/hotel-RoomStatus" replace />} />
+                  <Route
+                    path="/"
+                    element={<Navigate to="/hotel-RoomStatus" replace />}
+                  />
 
                   {/* Hotel routes */}
                   <Route path="/hotel" element={<HotelUserProfile />} />
@@ -338,242 +320,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-// import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Navigate,
-// } from "react-router-dom";
-// import { AuthProvider, useAuth } from "./auth/AuthContext";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
-// import "./styles/global.css";
-
-// import Login from "./Pages/Login";
-// import Register from "./Pages/Register";
-// import AllGuestPayments from "./Pages/AllGuestPayments";
-
-// import Sidebar from "./components/Sidebar";
-// import Navbar from "./components/Navbar";
-
-// import HotelUserProfile from "./Hotel_Admin_panel/UserProfile";
-// import HotelPage from "./Hotel_Admin_panel/Hotels";
-// import HotelRooms from "./Hotel_Admin_panel/Rooms";
-// import HotelGuestDetails from "./Hotel_Admin_panel/GuestDetails";
-// import HotelPayments from "./Hotel_Admin_panel/PaymentaPage";
-// import HotelReviews from "./Hotel_Admin_panel/Reviews";
-// import HotelRoomStatus from "./Hotel_Admin_panel/RoomStatus";
-
-// import HomestayUserProfile from "./Homestay_Admin_panel/UserProfile";
-// import HomestaysPage from "./Homestay_Admin_panel/Homestay";
-// import HomestaysRooms from "./Homestay_Admin_panel/Rooms";
-// import HomestaysHomestays from "./Homestay_Admin_panel/GuestDetails";
-// import HomestaysPayment from "./Homestay_Admin_panel/PaymentaPage";
-// import HomestaysReviews from "./Homestay_Admin_panel/Reviews";
-// import HomestaysRoomStatus from "./Homestay_Admin_panel/RoomStatus";
-
-// // Layout wrapper with sidebar & navbar
-// function Layout({ children }) {
-//   const [sidebarOpen, setSidebarOpen] = React.useState(false);
-
-//   const toggleSidebar = () => setSidebarOpen((prev) => !prev);
-
-//   return (
-//     <div className="App">
-//       <Sidebar isOpen={sidebarOpen} toggle={toggleSidebar} />
-//       <div className="main-content">
-//         <Navbar toggleSidebar={toggleSidebar} />
-//         {children}
-//       </div>
-//     </div>
-//   );
-// }
-
-// // Route protection
-// function PrivateRoute({ children }) {
-//   const { user, loading } = useAuth();
-
-//   if (loading) return <div>Loading...</div>;
-
-//   return user ? <Layout>{children}</Layout> : <Navigate to="/login" replace />;
-// }
-
-// function PublicRoute({ children }) {
-//   const { user, loading } = useAuth();
-
-//   if (loading) return <div>Loading...</div>;
-
-//   return user ? <Navigate to="/" replace /> : children;
-// }
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <Router>
-//         <Routes>
-//           {/* Public routes */}
-//           <Route
-//             path="/login"
-//             element={
-//               <PublicRoute>
-//                 <Login />
-//               </PublicRoute>
-//             }
-//           />
-//           <Route
-//             path="/register"
-//             element={
-//               <PublicRoute>
-//                 <Register />
-//               </PublicRoute>
-//             }
-//           />
-
-//           {/* Private routes */}
-//           <Route
-//             path="/"
-//             element={
-//               <PrivateRoute>
-//                 <Navigate to="/hotel-RoomStatus" replace />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/hotel"
-//             element={
-//               <PrivateRoute>
-//                 <HotelUserProfile />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/hotel-Page"
-//             element={
-//               <PrivateRoute>
-//                 <HotelPage />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/hotel-Rooms"
-//             element={
-//               <PrivateRoute>
-//                 <HotelRooms />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/hotel-GuestDetails"
-//             element={
-//               <PrivateRoute>
-//                 <HotelGuestDetails />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/hotel-Payments"
-//             element={
-//               <PrivateRoute>
-//                 <HotelPayments />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/hotel-Reviews"
-//             element={
-//               <PrivateRoute>
-//                 <HotelReviews />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/hotel-RoomStatus"
-//             element={
-//               <PrivateRoute>
-//                 <HotelRoomStatus />
-//               </PrivateRoute>
-//             }
-//           />
-
-//           {/* Homestay routes */}
-//           <Route
-//             path="/homestay"
-//             element={
-//               <PrivateRoute>
-//                 <HomestayUserProfile />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/homestay-Page"
-//             element={
-//               <PrivateRoute>
-//                 <HomestaysPage />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/homestay-Rooms"
-//             element={
-//               <PrivateRoute>
-//                 <HomestaysRooms />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/homestay-GuestDetails"
-//             element={
-//               <PrivateRoute>
-//                 <HomestaysHomestays />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/homestay-Payment"
-//             element={
-//               <PrivateRoute>
-//                 <HomestaysPayment />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/homestay-Reviews"
-//             element={
-//               <PrivateRoute>
-//                 <HomestaysReviews />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/homestay-RoomStatus"
-//             element={
-//               <PrivateRoute>
-//                 <HomestaysRoomStatus />
-//               </PrivateRoute>
-//             }
-//           />
-
-//           {/* Other routes */}
-//           <Route
-//             path="/AllGuestPayments"
-//             element={
-//               <PrivateRoute>
-//                 <AllGuestPayments />
-//               </PrivateRoute>
-//             }
-//           />
-
-//           {/* Catch-all redirect */}
-//           <Route path="*" element={<Navigate to="/" replace />} />
-//         </Routes>
-//       </Router>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
